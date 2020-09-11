be89906ec77a1fae91846a2bb52d575c10b75ce0
remneva <Pi775@mr>
2020-09-11 08:43:46 +0300

    HW2 fix lint

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -23,27 +23,20 @@ func Unpack(s string) (string, error) {
 				size := len(result)
 				result = result[:size-1]
 			} else {
-
 				if i == 0 {
-	//				fmt.Println("Incoming string is invalid")
 					return "", ErrInvalidString
 				}
 				_, err := strconv.Atoi(string([]rune(s)[i-1]))
 				if err == nil {
-	//				fmt.Println("Incoming string with two numeric is invalid")
 					return "", ErrInvalidString
 				}
-
 				g = strings.Repeat(string([]rune(s)[i-1]), b-1)
 
-				result = result + g
+				result += g
 			}
-
 		} else {
-			result = result + string(v)
+			result += string(v)
 		}
 	}
-
-//	fmt.Printf("%+q", result)
-	return result, nil
+	return result, ErrInvalidString
 }
