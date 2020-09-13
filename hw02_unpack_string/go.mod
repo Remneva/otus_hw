d56bad50600c4c2a16cc3f4d22201cc62d881bc1
remneva <Pi775@mr>
2020-09-13 12:36:47 +0300

    fix func

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,4 +1,4 @@
-module github.com/Remneva/hw02_unpack_string
+module github.com/Remneva/otus_hw/hw02_unpack_string
 
 go 1.14
 
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -13,30 +13,30 @@ func Unpack(s string) (string, error) {
 	var result string
 
 	for i, v := range []rune(s) {
-
 		var g string
 		if unicode.IsDigit(v) {
-
 			b, _ := strconv.Atoi(string(v))
 
-			if b == 0 {
+			if i == 0 { // проверка первого символа - первый символ не должен быть интом
+				return "", ErrInvalidString
+			}
+			_, err := strconv.Atoi(string([]rune(s)[i-1])) // проверка предыдущего символа - не должно ыть два инта подряд
+
+			if err == nil {
+				return "", ErrInvalidString
+			}
+			if b == 0 { // если инт = 0, не повторять букву ни одного раза
 				size := len(result)
 				result = result[:size-1]
-			} else {
-				if i == 0 {
-					return "", ErrInvalidString
-				}
-				_, err := strconv.Atoi(string([]rune(s)[i-1]))
-				if err == nil {
-					return "", ErrInvalidString
-				}
-				g = strings.Repeat(string([]rune(s)[i-1]), b-1)
 
+			} else {
+				g = strings.Repeat(string([]rune(s)[i-1]), b-1)
 				result += g
 			}
+
 		} else {
 			result += string(v)
 		}
 	}
-	return result, ErrInvalidString
+	return result, nil
 }
