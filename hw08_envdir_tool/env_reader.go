0738f94785e07c13970a9cba89e44cc931996ab9
remneva <Pi775@mr>
2020-11-08 20:18:12 +0300

    HW08 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,10 +1,43 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
 type Environment map[string]string
 
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
 	// Place your code here
-	return nil, nil
+	list := make(map[string]string)
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	for _, f := range files {
+		name := f.Name()
+		if !strings.Contains(name, "=") {
+			file, err := os.OpenFile(dir+"/"+name, os.O_RDONLY, 0755)
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer file.Close()
+			scanner := bufio.NewScanner(file)
+			scanner.Scan()
+			list[name] = scanner.Text()
+
+			if err := scanner.Err(); err != nil {
+				fmt.Println(name, " err: ", err)
+			}
+		}
+	}
+	return list, nil
 }
--- hw08_envdir_tool/go.mod
+++ hw08_envdir_tool/go.mod
@@ -1,3 +1,8 @@
-module github.com/fixme_my_friend/hw08_envdir_tool
+module github.com/Remneva/otus_hw/hw08_envdir_tool
 
 go 1.14
+
+require (
+	github.com/dchest/safefile v0.0.0-20151022103144-855e8d98f185
+	github.com/stretchr/testify v1.6.1
+)
