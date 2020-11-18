1cdc73239f6e04f605be27795b4c04832688f71b
remneva <Pi775@mr>
2020-11-18 08:00:31 +0300

    HW07 completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"io"
+	"os"
+	"time"
 )
 
 var (
@@ -10,6 +14,36 @@ var (
 )
 
 func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
+	time.Sleep(time.Second)
+	file, err := os.OpenFile(fromPath, os.O_RDWR, 0666)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer file.Close()
+	inf, _ := file.Stat()
+	buf := bufio.NewReaderSize(file, int(inf.Size()))
+
+	if int(offset) > buf.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	_, err = file.Seek(offset, io.SeekStart)
+
+	if err != nil {
+		return err
+	}
+	newFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	bw := bufio.NewWriter(newFile)
+
+	if limit == 0 || limit > int64(buf.Size()) {
+		limit = int64(buf.Size())
+	}
+	_, err = io.CopyN(bw, buf, limit)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,40 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here
+	t.Run("File.size less than offset", func(t *testing.T) {
+
+		content := []byte("temporary file's content")
+		tmpfile, err := ioutil.TempFile("", "test.")
+		if err != nil {
+			log.Println(err)
+		}
+		defer os.Remove(tmpfile.Name())
+		if _, err := tmpfile.Write(content); err != nil {
+			log.Println(err)
+		}
+
+		err = Copy(tmpfile.Name(), "/tmp/", 10000, 100)
+		if err != nil {
+			log.Println(err)
+		}
+		require.EqualError(t, err, ErrOffsetExceedsFileSize.Error())
+
+	})
+
+	t.Run("The infinite file unsupported", func(t *testing.T) {
+		err := Copy("dev/urandom", "testdata/expected.txt", int64(0), int64(0))
+		if err != nil {
+			log.Println(err)
+		}
+		require.EqualError(t, err, ErrUnsupportedFile.Error())
+
+	})
 }
