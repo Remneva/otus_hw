cd402c33aa541076707e0bb372c574a3f4b9dc65
remneva <Pi775@mr>
2020-12-26 18:39:50 +0300

    HW13 refactoring

--- hw12_13_14_15_calendar/internal/storage/memory/event_test.go
+++ hw12_13_14_15_calendar/internal/storage/memory/event_test.go
@@ -3,7 +3,6 @@ package memorystorage
 import (
 	"context"
 	"fmt"
-	"sync"
 	"testing"
 
 	"github.com/Remneva/otus_hw/hw12_13_14_15_calendar/internal/storage"
@@ -16,25 +15,13 @@ func TestMap(t *testing.T) {
 	ev1.Owner = 1
 	ev1.Title = "Title 1"
 	ev2.Title = "Title 2"
-	var events = []storage.Event{ev1, ev2}
 
 	t.Run("Events add to map", func(t *testing.T) {
 		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
-		wg := sync.WaitGroup{}
 		e := NewMap()
-		wg.Add(2)
-
-		for _, ev := range events {
-			go func(ev storage.Event) {
-				e.AddEvent(ctx, ev)
-				fmt.Println("done")
-				wg.Done()
-			}(ev)
-		}
-
-		wg.Wait()
-
+		_, err := e.AddEvent(ctx, ev1)
+		_, err = e.AddEvent(ctx, ev2)
 		actual1, err1 := e.GetEvent(ctx, 1)
 		actual2, err2 := e.GetEvent(ctx, 2)
 		fmt.Println("err1: ", err1)
@@ -45,7 +32,7 @@ func TestMap(t *testing.T) {
 		require.Contains(t, actual1.Title, "Title")
 		require.Contains(t, actual2.Title, "Title")
 
-		err := e.DeleteEvent(ctx, 1)
+		err = e.DeleteEvent(ctx, 1)
 		if err != nil {
 			require.NoError(t, err)
 		}
