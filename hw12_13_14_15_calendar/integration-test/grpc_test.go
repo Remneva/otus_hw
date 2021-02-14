af66bb36b0795eacf5f7c44af07be13e94fb8d6b
remneva <Pi775@mr>
2021-02-14 14:19:06 +0300

    HW15 docker-compose scripts

--- hw12_13_14_15_calendar/integration-test/grpc_test.go
+++ hw12_13_14_15_calendar/integration-test/grpc_test.go
@@ -16,12 +16,12 @@ import (
 )
 
 func TestServerGRPC(t *testing.T) {
-	start := time.Date(2009, 1, 1, 0, 0, 0, 0, time.UTC)
+	start := time.Date(2010, 1, 1, 0, 0, 0, 0, time.UTC)
 	starttime, err := ptypes.TimestampProto(start)
 	oneDayLater := start.AddDate(0, 0, 1)
 	endtime, err := ptypes.TimestampProto(oneDayLater)
 	conn, err := grpc.Dial("calendar:50051", grpc.WithInsecure())
-
+	ctx := context.Background()
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -35,7 +35,7 @@ func TestServerGRPC(t *testing.T) {
 		Description: "result",
 		Startdate:   "2020-03-01",
 		Starttime:   starttime,
-		Enddate:     "2020-03-01",
+		Enddate:     "2020-03-02",
 		Endtime:     endtime,
 	}
 
@@ -48,32 +48,47 @@ func TestServerGRPC(t *testing.T) {
 		assert.NotNil(t, respId)
 		fmt.Println("respId", respId.Id)
 		request.Id = respId.Id
-		respId, err = client.UpdateEvent(context.Background(), request)
-		if err != nil {
-			fmt.Printf("fail to dial: %v\n", err)
-		}
+		//req := &pb.Event{
+		//	Id:          respId.Id,
+		//	Title:       "Title",
+		//	Description: "result",
+		//	Startdate:   "2021-03-01",
+		//	Starttime:   starttime,
+		//	Enddate:     "2021-03-02",
+		//	Endtime:     endtime,
+		//}
+		//respId, err = client.UpdateEvent(ctx, req)
+		//if err != nil {
+		//	fmt.Printf("fail to dial: %v\n", err)
+		//}
+		//require.NoError(t, err)
 		id := &pb.Id{
-			Id: request.Id,
+			Id: respId.Id,
 		}
-		respEvent, err := client.GetEvent(context.Background(), id)
+		fmt.Println("get req Id", respId.Id)
+		respEvent, err := client.GetEvent(ctx, id)
 		if err != nil {
 			fmt.Printf("fail to dial: %v\n", err)
 		}
+		require.NoError(t, err)
+
 		//		assert.Equal(t, int64(1), respEvent.Owner)
 		assert.Equal(t, "Title", respEvent.Title)
 		assert.Equal(t, "result", respEvent.Description)
 		assert.Equal(t, "2020-03-01T00:00:00Z", respEvent.Startdate)
-		assert.Equal(t, "2020-03-01T00:00:00Z", respEvent.Enddate)
+		assert.Equal(t, "2020-03-02T00:00:00Z", respEvent.Enddate)
 
-		respEmpty, err := client.DeleteEvent(context.Background(), id)
+		respEmpty, err := client.DeleteEvent(ctx, id)
 		if err != nil {
 			fmt.Printf("fail")
 		}
+		require.NoError(t, err)
 		assert.Equal(t, emptypb.Empty{}, respEmpty)
 
 		request.Id = 0
 		respId, err = client.UpdateEvent(context.Background(), request)
-		e, _ := status.FromError(err)
+		e, ok := status.FromError(err)
+		require.False(t, ok)
 		fmt.Println(e.Code().String())
 		assert.Equal(t, codes.InvalidArgument, e.Code())
 
